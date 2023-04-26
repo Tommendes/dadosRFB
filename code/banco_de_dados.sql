fa60b3491c307de4c73b343e91749c6498b21641
Tom Mendes <contato@tommendes.com.br>
2023-04-26 15:39:36 -0300

    first commit

---
+++ code/banco_de_dados.sql
@@ -0,0 +1,12 @@
+-- Criar a base de dados "dados_rfb"
+CREATE DATABASE "dados_rfb"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "dados_rfb"
+    IS 'Base de dados para gravar os dados públicos de CNPJ da Receita Federal do Brasil';
+
+-- Diretório físico do banco de dados:
+--SHOW data_directory;
